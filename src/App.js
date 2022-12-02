b3e580b67aa8c0655cd9b1a9cbc040146735165d
developer-sora <sodaoov@gmail.com>
2022-12-02 19:24:31 +0900

    Feat: 2048 game final

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import AboveGame from "./component/AboveGame";
+import Game from "./component/Game";
+import Header from "./component/Header";
+import useLocalStorage from "./hook/useLocalStorage";
+
+function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useLocalStorage("bestScore", 0);
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+  });
+  return (
+    <div className="container">
+      <Header score={score} bestScore={bestScore} />
+      <AboveGame />
+      <Game setScore={setScore} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/util/number.js
@@ -0,0 +1,5 @@
+export function getRandomInteger(from, to) {
+  return Math.floor(Math.random() * to + from);
+}
+
+// 1부터 타일 가로/세로 길이 까지 랜덤한 수 return 
